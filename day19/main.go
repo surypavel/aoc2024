54305c75874e2eed6c53a0a52ae3428e1cd7e8bb
Pavel Surý <pavel.sury@rossum.ai>
2024-12-19 17:35:16 +0100

    Day 19 part 2

--- day19/main.go
+++ day19/main.go
@@ -12,13 +12,34 @@ func check(e error) {
 	}
 }
 
-func attainable(pattern string, towels []string) bool {
+func count_attainable(pattern string, towels []string, sum int, cache map[string]int) int {
+	if pattern == "" {
+		return 1
+	} else {
+		count := -1
+		if cache[pattern] != 0 {
+			count = cache[pattern]
+		} else {
+			count = 0
+			for _, towel := range towels {
+				if strings.HasPrefix(pattern, towel) {
+					count += count_attainable(pattern[len(towel):], towels, 1, cache)
+				}
+			}
+			cache[pattern] = count
+		}
+
+		return sum * count
+	}
+}
+
+func is_attainable(pattern string, towels []string) bool {
 	if pattern == "" {
 		return true
 	} else {
 		for _, towel := range towels {
 			if strings.HasPrefix(pattern, towel) {
-				if attainable(pattern[len(towel):], towels) {
+				if is_attainable(pattern[len(towel):], towels) {
 					return true
 				}
 			}
@@ -28,19 +49,27 @@ func attainable(pattern string, towels []string) bool {
 }
 
 func main() {
-	input, err := os.ReadFile("example.txt")
+	input, err := os.ReadFile("input.txt")
 	check(err)
 
 	input_parts := strings.Split(string(input), "\n\n")
 	towels := strings.Split(input_parts[0], ", ")
 	patterns := strings.Split(input_parts[1], "\n")
 
-	sum := 0
+	sum_1 := 0
 	for _, pattern := range patterns {
-		if attainable(pattern, towels) {
-			sum++
+		if is_attainable(pattern, towels) {
+			sum_1++
 		}
 	}
 
-	fmt.Print("part 1 - ", sum, "\n")
+	fmt.Print("part 1 - ", sum_1, "\n")
+
+	sum_2 := 0
+	cache := make(map[string]int)
+	for _, pattern := range patterns {
+		sum_2 += count_attainable(pattern, towels, 1, cache)
+	}
+
+	fmt.Print("part 1 - ", sum_2, "\n")
 }
